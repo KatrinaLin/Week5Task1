101de69454bd5c0a9607873b80da82eae677d5c6
Katrina <zhaoyanlin2016@gmail.com>
2018-04-21 09:29:23 -0700

    Extract the price calculation methods in PriceCalculator class.

--- src/main/java/practice3/Order.java
+++ src/main/java/practice3/Order.java
@@ -16,24 +16,8 @@ public class Order {
     }
 
     public BigDecimal calculate() {
-        BigDecimal subTotal = new BigDecimal(0);
+        PriceCaculator pc = new PriceCaculator();
 
-        // Total up line items
-        for (OrderLineItem lineItem : orderLineItemList) {
-            subTotal = subTotal.add(lineItem.getPrice());
-        }
-
-        // Subtract discounts
-        for (BigDecimal discount : discounts) {
-            subTotal = subTotal.subtract(discount);
-        }
-
-        // calculate tax
-        BigDecimal tax = subTotal.multiply(this.tax);
-
-        // calculate GrandTotal
-        BigDecimal grandTotal = subTotal.add(tax);
-
-        return grandTotal;
+        return pc.calculate(orderLineItemList, discounts, tax);
     }
 }
--- src/main/java/practice3/PriceCaculator.java
+++ src/main/java/practice3/PriceCaculator.java
@@ -1,4 +1,46 @@
 package practice3;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class PriceCaculator {
+
+    public BigDecimal getPriceTotal(List<OrderLineItem> orderLineItemList) {
+        BigDecimal subTotal = new BigDecimal(0);
+
+        // Total up line items
+        for (OrderLineItem lineItem : orderLineItemList) {
+            subTotal = subTotal.add(lineItem.getPrice());
+        }
+
+        return subTotal;
+    }
+
+    public BigDecimal getDiscountedTotal(List<BigDecimal> discounts) {
+        BigDecimal discountTotal = new BigDecimal(0);
+
+        // get discount total
+        for (BigDecimal discount : discounts) {
+             discountTotal = discountTotal.add(discount);
+        }
+
+        return discountTotal;
+    }
+
+    public BigDecimal getTaxTotal(BigDecimal subTotal, BigDecimal tax) {
+        // calculate tax
+        BigDecimal taxTotal = subTotal.multiply(tax);
+
+        return taxTotal;
+    }
+
+    public BigDecimal calculate(List<OrderLineItem> orderLineItemList, List<BigDecimal> discounts, BigDecimal tax) {
+
+        BigDecimal subTotal = getPriceTotal(orderLineItemList).subtract(getDiscountedTotal(discounts));
+
+        // calculate GrandTotal
+        BigDecimal grandTotal = subTotal.add(getTaxTotal(subTotal, tax));
+
+        return grandTotal;
+    }
 }
